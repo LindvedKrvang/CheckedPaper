531a898b309892d3f7d5d34e3893f836c9ef698b
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-02-07 21:08:03 +0100

    Added validation on numbers in MenuActivity

--- app/src/main/java/com/krvang/lindved/checkedpaper/gui/MenuActivity.java
+++ app/src/main/java/com/krvang/lindved/checkedpaper/gui/MenuActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.krvang.lindved.checkedpaper.R;
 import com.krvang.lindved.checkedpaper.gui.GameActivity;
@@ -23,8 +24,12 @@ public class MenuActivity extends AppCompatActivity {
 
     public void goToGameActivity(View view){
         Intent intent = new Intent(this, GameActivity.class);
-        intent.putExtra(getString(R.string.rows), Integer.parseInt(mRows.getText().toString()));
-        intent.putExtra(getString(R.string.columns), Integer.parseInt(mColumns.getText().toString()));
-        startActivity(intent);
+        try{
+            intent.putExtra(getString(R.string.rows), Integer.parseInt(mRows.getText().toString()));
+            intent.putExtra(getString(R.string.columns), Integer.parseInt(mColumns.getText().toString()));
+            startActivity(intent);
+        }catch (NumberFormatException nfe){
+            Toast.makeText(this, R.string.invalid_numbers, Toast.LENGTH_LONG).show();
+        }
     }
 }
