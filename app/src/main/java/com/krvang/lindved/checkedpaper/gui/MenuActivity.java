12ebfa3b2b3394d83aa30948443ab83e22e0b8d5
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-02-08 10:43:12 +0100

    Added a dummy menu

--- app/src/main/java/com/krvang/lindved/checkedpaper/gui/MenuActivity.java
+++ app/src/main/java/com/krvang/lindved/checkedpaper/gui/MenuActivity.java
@@ -3,6 +3,9 @@ package com.krvang.lindved.checkedpaper.gui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,5 +34,28 @@ public class MenuActivity extends AppCompatActivity {
         }catch (NumberFormatException nfe){
             Toast.makeText(this, R.string.invalid_numbers, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_hello:{
+                Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.menu_world:{
+                Toast.makeText(this, "World", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
